b00470d0beca5d800d1dc1bb39f31352afe7e619
kezdetphia <fehermark88@gmail.com>
2023-10-06 16:52:11 -0700

    index title

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,6 +20,7 @@ export const Contact = () => {
     phone: "",
     message: "",
   };
+  
   const [formDetails, setFormDetails] = useState(formInitialDetails);
   const [buttonText, setButtonText] = useState("Send");
   const [status, setStatus] = useState({});
